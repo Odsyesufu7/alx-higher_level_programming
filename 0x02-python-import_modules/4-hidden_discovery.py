c7737f301b793d0e7d2bd8542a011fb8fc14f744
OLUWASEYI DAUDA YESUFU <102301571+Odsyesufu7@users.noreply.github.com>
2022-06-30 19:54:53 +0100

    My fourth commit msg

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 import hidden_4
 
@@ -6,4 +5,3 @@ if __name__ == '__main__':
     names = [name for name in dir(hidden_4) if name[:2] != "__"]
     for name in names:
         print(name)
-
